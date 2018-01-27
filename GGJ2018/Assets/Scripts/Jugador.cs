b4e05950161f87b05829956bb56015ac9398aee6
sevic69 <sebdeveloper6952@gmail.com>
2018-01-27 09:57:41 -0600

    Se agrego interfaz.

---
+++ GGJ2018/Assets/Scripts/Jugador.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Jugador
+{
+    void salto();
+    void block();
+    void lightAttack();
+    void heavyAttack();
+    void dab();
+    void vergazo();
+}
