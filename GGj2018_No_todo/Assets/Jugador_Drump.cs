c9e48f21908f693918c23fd5de81f8fdcb7db729
Mario Galvez <maritogal@hotmail.com>
2018-01-27 15:03:31 -0600

    heavy

--- GGj2018_No_todo/Assets/Jugador_Drump.cs
+++ GGj2018_No_todo/Assets/Jugador_Drump.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 
 public class Jugador_Drump : MonoBehaviour {
+
+    private Jugador jugador { get { return FindObjectOfType<Jugador>(); } set { jugador = value; } }
+
+
+
+    private void Update()
+    {
+        heavyAttack();
+    } 
+
+
     void heavyAttack()
     {
-
+        if (jugador.movement.isBlocking == true)
+        {
+            return;
+        }
+        if (Input.GetButton("Heavy"))
+        {
+            //Aqui hacen lo que uds quieran
+        }
     }
 
 }
