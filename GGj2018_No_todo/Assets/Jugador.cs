eeaaa57247feb85463fd83cf3f977e72df739af1
Mario Galvez <maritogal@hotmail.com>
2018-01-27 14:23:31 -0600

    jugador

---
+++ GGj2018_No_todo/Assets/Jugador.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(BoxCollider2D))]
+
+
+public class Jugador : MonoBehaviour
+{
+
+    Animator animator;
+
+
+    [System.Serializable]
+    public class AnimationSettings
+    {
+        public string horizontalVelocityFloat = "Horizontal";
+        public string jumpBool = "jump";
+        public string groundedBool = "isGrounded";
+        public string lightAttack = "lightAttack";
+        public string heavyAttack = "heavyAttack";
+        public string block = "block";
+        public string dab = "dab";
+    }
+    [SerializeField]
+    public AnimationSettings animations;
+
+    [System.Serializable]
+    public class MovimientoSettings
+    {
+        public bool isBlocking = false;
+        public float factorVelocidad = 21.3f;
+    }
+    [SerializeField]
+    public MovimientoSettings movement;
+
+    [System.Serializable]
+    public class JumpSettings
+    {
+        public Transform groundPoint;
+        public LayerMask groundMask;
+        public float groundDetectionDistance = 0.1f;
+        public float jumpForce = 5.0f;
+    }
+    [SerializeField]
+    public JumpSettings jump;
+
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private bool isTouchingGround;
+
+    Vector3 jugadorTras;
+    private int direccion;
+
+    // Use this for initialization
+    void Start()
+    {
+        jugadorTras = new Vector3(0.0f, 0.0f, 0.0f);
+
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movimiento();
+        salto();
+        block();
+
+        // detectar si el jugador esta tocando el piso
+        DetectPlayerTouchingGround();
+    }
+
+    void movimiento()
+    {
+        if (movement.isBlocking == true)
+        {
+            return;
+        }
+
+        jugadorTras.y = 0;
+        jugadorTras.x = 0;
+        jugadorTras.z = 0;
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            jugadorTras.x = movement.factorVelocidad * Time.deltaTime;
+            transform.Translate(jugadorTras);
+            direccion = 1;
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            jugadorTras.x = -movement.factorVelocidad * Time.deltaTime;
+            transform.Translate(jugadorTras);
+            direccion = -1;
+        }
+
+    }
+    void salto()
+    {
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            if (isTouchingGround)
+            {
+                rb.AddForce(Vector2.up * jump.jumpForce, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private void DetectPlayerTouchingGround()
+    {
+        isTouchingGround = Physics2D.OverlapCircle(jump.groundPoint.position, jump.groundDetectionDistance,
+            jump.groundMask);
+
+        if (isTouchingGround)
+        {
+            //animator.SetBool("Jumping", false);
+        }
+        //else animator.SetBool("Jumping", true);
+    }
+    void block()
+    {
+        movement.isBlocking = false;
+
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            movement.isBlocking = true;
+
+        }
+    }
+    void lightAttack() { }
+    void dab() { }
+    void vergazo()
+    {
+        if (movement.isBlocking == true)
+        {
+            return;
+        }
+    }
+}
--- GGj2018_No_todo/Assets/Jugador_Drump.cs
+++ GGj2018_No_todo/Assets/Jugador_Drump.cs
@@ -2,45 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Jugador_Drump : MonoBehaviour {
-
-    Vector3 jugadorTras;
-    float factorVelocidad;
-    public int jugador;
-
-    // Use this for initialization
-    void Start () {
-        jugadorTras = new Vector3(0.0f, 0.6f, 0.0f);
-        factorVelocidad = 21.3f;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        movimiento();
-	}
-
-    void movimiento()
+    void heavyAttack()
     {
-        jugadorTras.y = 0;
-        jugadorTras.x = 0;
-        jugadorTras.z = 0;
-        
-        if (Input.GetAxis("Horizontal") > 0)
-        {
-            jugadorTras.x = factorVelocidad * Time.deltaTime;
-            transform.Translate(jugadorTras);
-        }
-        if (Input.GetAxis("Horizontal") < 0)
-        {
-            jugadorTras.x = -factorVelocidad * Time.deltaTime;
-            transform.Translate(jugadorTras);
-        }
 
     }
-    void salto() { }
-    void block() { }
-    void lightAttack() { }
-    void heavyAttack() { }
-    void dab() { }
-    void vergazo() { }
+
 }
