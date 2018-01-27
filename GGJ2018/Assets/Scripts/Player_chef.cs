c06351af8011cba325cc0363ff8e47e2a4be90d4
DaronP <per16362@uvg.edu.gt>
2018-01-27 10:28:57 -0600

    Arena

---
+++ GGJ2018/Assets/Scripts/Player_chef.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Player_chef
+{
+	void salto();
+	void block();
+	void lightAttack();
+	void heavyAttack();
+	void dab();
+	void vergazo();
+}
