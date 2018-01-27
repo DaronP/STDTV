d0e8a9843751f6abb696e65ce28f94ddcca298e7
DaronP <per16362@uvg.edu.gt>
2018-01-27 10:04:47 -0600

    Juego

---
+++ GGJ2018/Assets/Scripts/Menus/QuitGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour {
+
+	// Use this for initialization
+	public void Quit()
+	{
+		UnityEditor.EditorApplication.isPlaying = false;
+		//Application.Quit ();
+	}
+}
